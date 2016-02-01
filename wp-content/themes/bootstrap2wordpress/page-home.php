87448c678a79fc57691ec30a7782aaa29936244c
Kat Reinhart <kat.reinhart@gmail.com>
2016-01-31 16:15:01 -0800

    Added advanced custom fields and coded Income section dynamically

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -4,13 +4,23 @@
  */
 
 // Custom Fields
-$prelaunch_price     = get_post_meta( 7, 'prelaunch_price', true );
-$launch_price        = get_post_meta( 7, 'launch_price', true );
-$final_price         = get_post_meta( 7, 'final_price', true );
-$course_url          = get_post_meta( 7, 'course_url', true);
-$button_text         = get_post_meta( 7, 'button_text', true);
-$optin_text          = get_post_meta( 7, 'optin_text', true);
-$optin_button_text   = get_post_meta( 7, 'optin_button_text', true);
+$prelaunch_price           = get_post_meta( 7, 'prelaunch_price', true );
+$launch_price              = get_post_meta( 7, 'launch_price', true );
+$final_price               = get_post_meta( 7, 'final_price', true );
+$course_url                = get_post_meta( 7, 'course_url', true);
+$button_text               = get_post_meta( 7, 'button_text', true);
+$optin_text                = get_post_meta( 7, 'optin_text', true);
+$optin_button_text         = get_post_meta( 7, 'optin_button_text', true);
+
+// Advanced Custom Fields
+$income_feature_image       = get_field('income_feature_image');
+$income_section_title       = get_field('income_section_title');
+$income_section_desc        = get_field('income_section_description');
+$reason_1_title             = get_field('reason_1_title');
+$reason_1_desc              = get_field('reason_1_description');
+$reason_2_title             = get_field('reason_2_title');
+$reason_2_desc              = get_field('reason_2_description');
+
 
 get_header(); ?>
 
@@ -70,19 +80,23 @@ get_header(); ?>
     <div class="container">
 
       <div class="section-header">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
-        <h2>How you can boost your income</h2>
+        <!-- if feature image exists -->
+        <?php
+          if( !empty($income_feature_image) ) : ?>
+          <img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>">
+        <?php endif; ?>
+        <h2><?php echo $income_section_title; ?></h2>
 
       </div><!-- section header -->
-      <p class="lead">Whether you’re a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career — this course gives you an immensely valuable skill that will enable you to either:</p>
+      <p class="lead"><?php echo $income_section_desc; ?></p>
       <div class="row">
         <div class="col-sm-6">
-          <h3>Make money on the side</h3>
-          <p>So you can save up for that Hawaiian vacation you’ve been wanting, help pay off your debt, your car, your mortgage, or simply just to have bonus cash laying around.</p>
+          <h3><?php echo $reason_1_title; ?></h3>
+          <p><?php echo $reason_1_desc; ?></p>
         </div> <!-- column -->
         <div class="col-sm-6">
-          <h3>Create a full-time income</h3>
-          <p>WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.</p>
+          <h3><?php echo $reason_2_title; ?></h3>
+          <p><?php echo $reason_2_desc; ?></p>
         </div> <!-- column -->
       </div><!-- row -->
     </div><!-- container -->
