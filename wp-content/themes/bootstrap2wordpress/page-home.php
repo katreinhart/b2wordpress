0a2a3efb33be8dfe38a3f58e14d029cbf2c45b61
Kat Reinhart <kat.reinhart@gmail.com>
2016-01-31 17:24:03 -0800

    Add custom post types UI plugin and create course feature post

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -16,11 +16,19 @@ $optin_button_text         = get_post_meta( 7, 'optin_button_text', true);
 $income_feature_image       = get_field('income_feature_image');
 $income_section_title       = get_field('income_section_title');
 $income_section_desc        = get_field('income_section_description');
+
 $reason_1_title             = get_field('reason_1_title');
 $reason_1_desc              = get_field('reason_1_description');
 $reason_2_title             = get_field('reason_2_title');
 $reason_2_desc              = get_field('reason_2_description');
 
+$who_feature_img            = get_field('who_feature_img');
+$who_section_title          = get_field('who_section_title');
+$who_section_body           = get_field('who_section_body');
+
+$features_section_image     = get_field('features_section_image');
+$features_section_title     = get_field('features_section_title');
+$features_section_body      = get_field('features_section_body');
 
 get_header(); ?>
 
@@ -107,26 +115,16 @@ get_header(); ?>
   <section id="who-benefits">
     <div class="container">
       <div class="section-header">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
-        <h2>Who Should Take This Course?</h2>
+        <?php
+          if( !empty($who_feature_img ) ) : ?>
+          <img src="<?php echo $who_feature_img['url']; ?>" alt="<?php echo $who_feature_img ['alt']; ?>">
+        <?php endif; ?>
+        <h2><?php echo $who_section_title; ?></h2>
       </div> <!-- section-header -->
       <div class="row">
 
         <div class="col-sm-8 col-sm-offset-2">
-          <h3>Graphic &amp; Web Designers</h3>
-          <p>Graphic designers are extremely talented, but ask them to code their designs and they’ll freeze up! This leaves them with no other choice but to hire a web developer. Any professional graphic designers knows web developers can be expensive.
-If you’re a designer, learning to code your own WordPress websites can change your business entirely! Now, not only are you a great designer, but you’re a skillful developer, too! This puts you in a position to make an extra <strong>$1,000 – $5,000</strong> per project.</p>
-          <h3> Entrepreneurs</h3>
-          <p>Entrepreneurs have big dreams, and in many cases, shoestring budgets. In order to survive in the cut-throat world of the Startup company, it’s a necessity to have a world-class website. However, world-class websites come with a large price tag.
-If you can learn how to build a high-quality startup website by yourself, then you’ve just saved yourself a lot of cash, <strong>tens of thousands of dollars in many cases.</strong></p>
-          <h3>Employees</h3>
-          <p>Any company knows the education &amp; training of their employees is key to a thriving team.
-Depending on the type of company you work for, if you understand how to code, and can develop CMS driven websites, that gives you <strong>negotiating power for a better position, or a higher salary.</strong></p>
-          <h3>Code Hobbyists</h3>
-          <p>It’s fun to learn challenging new skills. Code hobbyists can add dynamic websites to their arsenal of tools to play with — you can even sell WordPress themes and plugins for cash! The possibilities are <strong>truly endless.</strong></p>
-          <h3>People Looking for a New Career</h3>
-          <p>Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere in the world? Becoming a Web Developer might be the answer for you.
-Web developers are paid well, anywhere from $33,000 to more than $105,000 per year. They get to work at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!</p>
+          <?php echo $who_section_body ?>
         </div><!-- end column with content -->
       </div>
     </div> <!-- end container -->
@@ -138,35 +136,30 @@ Web developers are paid well, anywhere from $33,000 to more than $105,000 per ye
     <div class="container">
 
       <div class="section-header">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-        <h2>Course Features</h2>
+        <?php
+          if( !empty($who_feature_img ) ) : ?>
+          <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+        <?php endif; ?>
+        <h2><?php echo $features_section_title; ?></h2>
+        <?php
+          if( !empty($features_section_body ) ) : ?>
+          <p class="lead">
+            <?php echo $features_section_body; ?>
+          </p>
+        <?php endif; ?>
       </div><!-- section-header -->
 
       <div class="row">
-        <div class="col-sm-2">
-          <i class="ci ci-computer"></i>
-          <h4>Lifetime access to 80+ lectures</h4>
-        </div><!-- col -->
-        <div class="col-sm-2">
-          <i class="ci ci-watch"></i>
-          <h4>10+ hours of HD video content</h4>
-        </div><!-- col -->
-        <div class="col-sm-2">
-          <i class="ci ci-calendar"></i>
-          <h4>30 day money back guarantee</h4>
-        </div><!-- col -->
-        <div class="col-sm-2">
-          <i class="ci ci-community"></i>
-          <h4>Access to a community of like-minded students</h4>
-        </div><!-- col -->
-        <div class="col-sm-2">
-          <i class="ci ci-instructor"></i>
-          <h4>Direct access to the instructor</h4>
-        </div><!-- col -->
-        <div class="col-sm-2">
-          <i class="ci ci-device"></i>
-          <h4>Accessible content on your mobile devices</h4>
-        </div><!-- col -->
+
+        <?php $loop = new WP_Query ( array( 'post_type' => 'course_feature', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+          <div class="col-sm-2">
+            <i class="<?php the_field('course_feature_icon'); ?>"></i>
+            <h4><?php the_title(); ?></h4>
+          </div><!-- col -->
+        <?php endwhile; ?>
+
       </div><!-- row -->
     </div>   <!-- container -->
   </section>  <!-- course features -->
