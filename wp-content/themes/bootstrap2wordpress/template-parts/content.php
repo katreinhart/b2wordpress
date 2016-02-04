da736691bd358072b8fb4631e1f1cf58224a0088
Kat Reinhart <kat.reinhart@gmail.com>
2016-02-03 20:30:42 -0800

    Blog index and article pages

--- wp-content/themes/bootstrap2wordpress/template-parts/content.php
+++ wp-content/themes/bootstrap2wordpress/template-parts/content.php
@@ -9,39 +9,57 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+<div class="container">
+	<div class="row">
+		<div class="col-sm-8" id="primary">
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php bootstrap2wordpress_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php
+						if ( is_single() ) {
+							the_title( '<h3 class="entry-title">', '</h3>' );
+						} else {
+							the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+						}
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bootstrap2wordpress' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+					if ( 'post' === get_post_type() ) : ?>
+						<div class="post-details">
+							<i class="fa fa-user"></i> <?php the_author(); ?>
+							<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+							<i class="fa fa-folder"> </i><?php the_category(', '); ?>
+							<i class="fa fa-tags"></i> <?php the_tags('', ', ', ''); ?>
+							<div class="post-comments-badge">
+								<a href="#"><i class="fa fa-comments"></i><?php comments_number( 0, 1, '%'); ?></a>
+							</div>
+						</div> <!-- post-details -->
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+							<?php edit_post_link( 'Edit', '<div><i class="fa fa-pencil"></i>', '</div>' ); ?>
 
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+					<?php endif; ?>
+
+				</header><!-- .entry-header -->
+
+			<?php if ( has_post_thumbnail()) { ?>
+					<div class="post-image">
+						<?php the_post_thumbnail(); ?>
+					</div>
+					<?php } ?>
+					<div class="post-excerpt">
+						<?php the_content(); ?>
+					</div>
+			</article><!-- #post-## -->
+
+		</div><!-- primary col -->
+
+		<!-- SIDEBAR!
+		==============================================-->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+
+		</aside>
+
+	</div> <!-- row -->
+
+</div> <!-- container -->
+
+<?php get_footer(); ?>
