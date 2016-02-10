9c85d31818898f2afb14b8129dd9fed145226f6b
Kat Reinhart <kat.reinhart@gmail.com>
2016-02-09 19:42:10 -0800

    Fixed bug with broken sidebar

--- wp-content/themes/bootstrap2wordpress/index.php
+++ wp-content/themes/bootstrap2wordpress/index.php
@@ -18,49 +18,41 @@ get_header(); ?>
 	<h1 class="page-title">Blog</h1>
 </section>
 
+
+<!-- BLOG CONTENT =========================
+=========================================== -->
 <div class="container">
 		<div class="row" id="primary">
 
 			<main id="content" class="col-sm-8" role="main">
+				<?php	if ( have_posts() ) : ?>
 
-				<?php
-				if ( have_posts() ) :
+					<?php 		/* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post();  ?>
 
-					if ( is_home() && ! is_front_page() ) : ?>
-						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-						</header>
+						<?php 		/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */   ?>
+						<?php 	get_template_part( 'template-parts/content', get_post_format() );  ?>
 
-					<?php
-					endif;
+					<?php endwhile; //end while have posts ?>
 
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
+					<?php //	the_posts_navigation(); // this line was breaking the nav sidebar ?>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_format() );
+				<?php 	else : // if no posts  ?>
 
-					endwhile;
+					<?php 	get_template_part( 'template-parts/content', 'none' );  ?>
 
-					the_posts_navigation();
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif; ?>
+				<?php 	endif; // end if no posts ?>
 
 			</main>
 
 			<!-- SIDEBAR!
 			==============================================-->
-			<aside class="col-sm-4">
-				<?php get_sidebar(); ?>
-			</aside>
+			<?php get_sidebar(  ); ?>
+
 
 		</div><!-- row -->
 	</div><!--- container -->
